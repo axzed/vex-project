7af03907cbc98305384769d0b47df2549e2c7cda
axzed <waxwc1491224820@gmail.com>
2023-02-23 18:45:52 +0800

    :hammer: refactor: fix gorm ref

--- project-user/internal/data/member.go
+++ project-user/internal/data/member.go
@@ -2,29 +2,30 @@ package data
 
 // Member 成员表
 type Member struct {
-	Id              int64
-	Account         string
-	Password        string
-	Name            string
-	Mobile          string
-	Realname        string
-	CreateTime      int64
-	Status          int
-	LastLoginTime   int64
-	Sex             int
-	Avatar          string
-	Idcard          string
-	Province        int
-	City            int
-	Area            int
-	Address         string
-	Description     string
-	Email           string
-	DingtalkOpenid  string
-	DingtalkUnionid string
-	DingtalkUserid  string
+	Id              int64  `gorm:"id"`
+	Account         string `gorm:"account"`
+	Password        string `gorm:"password"`
+	Name            string `gorm:"name"`
+	Mobile          string `gorm:"mobile"`
+	Realname        string `gorm:"realname"`
+	CreateTime      int64  `gorm:"create_time"`
+	Status          int    `gorm:"status"`
+	LastLoginTime   int64  `gorm:"last_login_time"`
+	Sex             int    `gorm:"sex"`
+	Avatar          string `gorm:"avatar"`
+	Idcard          string `gorm:"idcard"`
+	Province        int    `gorm:"province"`
+	City            int    `gorm:"city"`
+	Area            int    `gorm:"area"`
+	Address         string `gorm:"address"`
+	Description     string `gorm:"description"`
+	Email           string `gorm:"email"`
+	DingtalkOpenid  string `gorm:"dingtalk_openid"`
+	DingtalkUnionid string `gorm:"dingtalk_unionid"`
+	DingtalkUserid  string `gorm:"dingtalk_userid"`
 }
 
+// TableName 映射表名
 func (*Member) TableName() string {
 	return "vex_member"
 }
--- project-user/internal/data/organization.go
+++ project-user/internal/data/organization.go
@@ -2,19 +2,20 @@ package data
 
 // Organization 组织表
 type Organization struct {
-	Id          int64
-	Name        string
-	Avatar      string
-	Description string
-	MemberId    int64
-	CreateTime  int64
-	Personal    int32
-	Address     string
-	Province    int32
-	City        int32
-	Area        int32
+	Id          int64  `gorm:"id"`
+	Name        string `gorm:"name"`
+	Avatar      string `gorm:"avatar"`
+	Description string `gorm:"description"`
+	MemberId    int64  `gorm:"member_id"`
+	CreateTime  int64  `gorm:"create_time"`
+	Personal    int32  `gorm:"personal"`
+	Address     string `gorm:"address"`
+	Province    int32  `gorm:"province"`
+	City        int32  `gorm:"city"`
+	Area        int32  `gorm:"area"`
 }
 
+// TableName 映射表名
 func (*Organization) TableName() string {
 	return "vex_organization"
 }
