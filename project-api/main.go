b1bb6077e0f955c64d12cc7f5c9e7e210dda0dd7
axzed <waxwc1491224820@gmail.com>
2023-04-04 20:39:41 +0800

    :zap: perf(pro): use net/http/pprof analyze the leak of memory and goroutine

--- project-api/main.go
+++ project-api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	_ "github.com/axzed/project-api/api"
 	"github.com/axzed/project-api/api/middleware"
 	"github.com/axzed/project-api/config"
@@ -9,6 +10,7 @@ import (
 	"github.com/gin-contrib/pprof"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -21,6 +23,38 @@ func main() {
 	router.InitRouter(r)
 	// 开启pprof 默认访问路径 /debug/pprof
 	pprof.Register(r)
+	//测试代码
+	r.GET("/mem", func(c *gin.Context) {
+		// 业务代码运行
+		outCh := make(chan int)
+		// 每秒起10个goroutine，goroutine会阻塞，不释放内存
+		tick := time.Tick(time.Second / 10)
+		i := 0
+		for range tick {
+			i++
+			fmt.Println(i)
+			alloc1(outCh) // 不停的有goruntine因为outCh堵塞，无法释放
+		}
+	})
 	// 将优雅启停抽取到common的Run中
 	common.Run(r, config.AppConf.SC.Name, config.AppConf.SC.Addr, nil)
+}
+
+// 一个外层函数
+func alloc1(outCh chan<- int) {
+	go alloc2(outCh)
+}
+
+// 一个内层函数
+func alloc2(outCh chan<- int) {
+	func() {
+		defer fmt.Println("alloc-fm exit")
+		// 分配内存，假用一下
+		buf := make([]byte, 1024*1024*10)
+		_ = len(buf)
+		fmt.Println("alloc done")
+
+		outCh <- 0
+		//return
+	}()
 }
